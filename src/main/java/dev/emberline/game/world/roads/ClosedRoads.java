f71338a5d08c8498d29807015413ac8c69ea5000
Luigi Linari <luigi.linari@studio.unibo.it>
2025-04-23 15:02:23 +0200

    deletion of the ClosedRoads class, since it does not seem to be useful in the model of the application

--- src/main/java/dev/emberline/game/world/roads/ClosedRoads.java
+++
@@ -1,6 +0,0 @@
-package dev.emberline.game.world.roads;
-
-public class ClosedRoads {
-    //stores all the roads that are yet to be opened
-    //once they open they shall be removed and sent to the Roads class
-}
