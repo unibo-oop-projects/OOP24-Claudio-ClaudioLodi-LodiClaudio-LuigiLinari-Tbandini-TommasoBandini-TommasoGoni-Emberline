d01506a8a569f06b744b08bf1e98bc4c33204245
Luigi Linari <luigi.linari@studio.unibo.it>
2025-04-23 14:58:02 +0200

    adjusted the wave and wavemanager class to the previous changes

--- src/main/java/dev/emberline/game/world/waves/Wave.java
+++ src/main/java/dev/emberline/game/world/waves/Wave.java
@@ -8,36 +8,20 @@ import java.util.Queue;
 import dev.emberline.core.components.Updatable;
 import dev.emberline.game.world.World;
 import dev.emberline.game.world.roads.Roads;
+import dev.emberline.game.world.spawnpoints.Spawnpoints;
 import javafx.geometry.Point2D;
 import utility.pairs.Pair;
 
 public class Wave implements Updatable {
     
     private World world;
-    private Roads roads = new Roads("filename");
-    private List<Queue<Pair<Pair<Integer,Integer>,Long>>> enemiesToSpawn;
+    private Roads roads = new Roads("./src/main/resources/loadingFiles/roads.txt");
+    private Spawnpoints spawnpoints = new Spawnpoints("./src/main/resources/loadingFiles/spawnpoints.txt");
     
-    //temp variables to delete later
-    private long timeFromStart = 5_000_000_000L;
-    private long c = 2_000_000_000L;
-    //(x,y) coords for example road (check roads, (2,6) should be the spawnpoint)
-    private int a = 2;
-    private int b = 10;
-
     private long acc = 0;
 
     public Wave(World world) {
         this.world = world;
-        
-        enemiesToSpawn = new ArrayList<>(1);
-        Queue<Pair<Pair<Integer,Integer>,Long>> tempQueue = new LinkedList<>(); 
-        tempQueue.add(new Pair<>(new Pair<>(a, b), timeFromStart));
-        tempQueue.add(new Pair<>(new Pair<>(a, b), timeFromStart+c));
-        tempQueue.add(new Pair<>(new Pair<>(a, b), timeFromStart+2*c));
-        tempQueue.add(new Pair<>(new Pair<>(a, b), timeFromStart+3*c));
-        tempQueue.add(new Pair<>(new Pair<>(a, b), timeFromStart+4*c));
-
-        enemiesToSpawn.add(tempQueue);
     }
 
 
@@ -53,14 +37,11 @@ public class Wave implements Updatable {
     }
 
     public void sendEnemies() {
-        for (int i = 0; i < enemiesToSpawn.size(); i++) {
-            Queue<Pair<Pair<Integer,Integer>,Long>> queue = enemiesToSpawn.get(i); 
-            while (!queue.isEmpty() && queue.peek().getY() <= acc) {
-                //will switch to a same type of pairs, and erase this since hence usless
-                Pair<Integer,Integer> p = queue.poll().getX();
-                Point2D p2 = new Point2D(p.getX(), p.getY());
-                world.getEnemiesManager().addEnemy(p2);
-            }
+        List<Pair<Integer,Integer>> enemiesQueue = spawnpoints.getEnemies(acc);
+        for (var enemy : enemiesQueue) {
+            //will switch to a same type of pairs, and erase this since hence usless
+            Point2D p2 = new Point2D(enemy.getX(), enemy.getY());
+            world.getEnemiesManager().addEnemy(p2);
         }
     }
 }
--- src/main/java/dev/emberline/game/world/waves/WaveManager.java
+++ src/main/java/dev/emberline/game/world/waves/WaveManager.java
@@ -2,12 +2,10 @@ package dev.emberline.game.world.waves;
 
 import dev.emberline.core.components.Updatable;
 import dev.emberline.game.world.World;
-import dev.emberline.game.world.roads.ClosedRoads;
 
 public class WaveManager implements Updatable {
     
     private World world;
-    private ClosedRoads closedRoads = new ClosedRoads();
     //this will be one of the n waves contained in the list
     private Wave currentWave;
     
