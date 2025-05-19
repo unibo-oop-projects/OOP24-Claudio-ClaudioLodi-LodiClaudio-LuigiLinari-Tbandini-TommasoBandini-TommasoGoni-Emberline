0ac08662b038a03e2e7b6957947f43c459c80e08
Luigi Linari <luigi.linari@studio.unibo.it>
2025-05-19 12:11:47 +0200

    added isOver method to wave class, and other changes need due to the distribution of input

--- src/main/java/dev/emberline/game/world/waves/Wave.java
+++ src/main/java/dev/emberline/game/world/waves/Wave.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
+import dev.emberline.core.GameLoop;
 import dev.emberline.core.components.Renderable;
 import dev.emberline.core.components.Updatable;
+import dev.emberline.core.render.CoordinateSystem;
+import dev.emberline.core.render.Renderer;
 import dev.emberline.game.world.World;
 import dev.emberline.game.world.roads.Roads;
 import dev.emberline.game.world.spawnpoints.Spawnpoints;
+import javafx.scene.canvas.GraphicsContext;
 import utility.Coordinate2D;
 import utility.IntegerPoint2D;
 import utility.Vector2D;
@@ -20,26 +24,24 @@ public class Wave implements Updatable, Renderable {
     //getResources() or similar
     private Roads roads;
     private Spawnpoints spawnpoints;
-    private String wavePath;
+    private CoordinateSystemManager csManager;
 
+    private double csIncrement = 0;
     private long acc = 0;
 
     public Wave(World world, String wavePath) {
         this.world = world;
-        this.wavePath = wavePath;
         roads = new Roads(wavePath);
         spawnpoints = new Spawnpoints(wavePath);
+        csManager = new CoordinateSystemManager(wavePath);
     }
 
     public Optional<IntegerPoint2D> getNext(IntegerPoint2D pos) {
         return roads.getNextNode(pos);
     }
 
-    @Override
-    public void update(long elapsed) {
-        acc += elapsed;
-
-        sendEnemies();
+    public boolean isOver() {
+        return world.getEnemiesManager().areAllDead() && spawnpoints.hasMoreToSpawn();
     }
 
     private void sendEnemies() {
@@ -48,6 +50,15 @@ public class Wave implements Updatable, Renderable {
             Vector2D p2 = new Coordinate2D(enemy.getX(), enemy.getY());
             world.getEnemiesManager().addEnemy(p2);
         }
+    }
+
+    @Override
+    public void update(long elapsed) {
+        acc += elapsed;
+
+        sendEnemies();
+
+        roads.update(elapsed);
     }
 
     @Override
