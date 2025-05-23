223ff5261efc7ffc308f9a0e4b1efc2a6d969656
Tommaso Goni <tommaso.goni@studio.unibo.it>
2025-05-23 22:35:38 +0200

    Introduce UpgradableInfo interface and refactor upgrade logic
    Added a reusable UpgradableInfo interface to standardize upgrade-related functionality. Refactored ProjectileInfo and EnchantmentInfo to implement this interface, consolidating upgrade methods and integrating them with TowerStatsProvider. Improved maintainability by encapsulating stats in nested static classes and aligning behavior with the new interface.

---
+++ src/main/java/dev/emberline/game/model/UpgradableInfo.java
@@ -0,0 +1,55 @@
+package dev.emberline.game.model;
+
+public interface UpgradableInfo<T extends UpgradableInfo.InfoType, SELF extends UpgradableInfo<T, SELF>> {
+    interface InfoType {}
+
+    /**
+     * Retrieves the maximum level to which the current object can be upgraded.
+     *
+     * @return the maximum upgrade level for this object.
+     */
+    int getMaxLevel();
+
+    /**
+     * Determines if the current object is eligible for an upgrade.
+     *
+     * @return true if the object can be upgraded; false otherwise.
+     */
+    boolean canUpgrade();
+
+    /**
+     * Determines if the current object type can be changed to a different type.
+     *
+     * @return true if the object type can be changed, false otherwise.
+     */
+    boolean canChangeType();
+
+    /**
+     * Returns a new instance of the same object upgraded to the next level.
+     * <p>
+     * Before calling this method, ensure that {@link #canUpgrade()} returns true.
+     *
+     * @return the upgraded version of the current object.
+     */
+    SELF getUpgrade();
+
+    /**
+     * Returns a new instance of the same object updated to the specified type.
+     * <p>
+     * Before calling this method, ensure that {@link #canChangeType()} returns true.
+     *
+     * @param type the target type to which the current instance should be changed.
+     * @return the updated version of the current object to the specified type.
+     */
+    SELF getChangeType(T type);
+
+    /**
+     * Calculates and retrieves the cost required for upgrading or transitioning from the base type to a specific type.
+     * <p>
+     * The presence of an upgrade cost does not guarantee that upgrading is possible, you must always check
+     * {@link #canUpgrade()} before attempting to upgrade or {@link #canChangeType()} before changing the type.
+     *
+     * @return The cost associated with either upgrading to the next level or switching from the base type.
+     */
+    int getUpgradeCost();
+}
