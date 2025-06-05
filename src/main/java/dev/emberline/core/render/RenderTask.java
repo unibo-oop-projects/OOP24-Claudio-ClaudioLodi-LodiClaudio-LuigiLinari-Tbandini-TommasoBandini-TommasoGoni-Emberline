8d45606a663385987ce355279bc3855a4ede4a3a
Tommaso Goni <tommaso.goni@studio.unibo.it>
2025-06-05 17:10:26 +0200

    Now RenderTasks with the same RenderPriority get rendered in the same order as the addRenderTask call order.

--- src/main/java/dev/emberline/core/render/RenderTask.java
+++ src/main/java/dev/emberline/core/render/RenderTask.java
@@ -4,6 +4,7 @@ import java.lang.Comparable;
 
 public class RenderTask implements Comparable<RenderTask>, Runnable {
     private final RenderPriority renderPriority;
+    private long secondaryPriority = 0; //lower values get rendered first
     private final Runnable runnable;
 
     public RenderTask(RenderPriority renderPriority, Runnable runnable) {
@@ -17,11 +18,24 @@ public class RenderTask implements Comparable<RenderTask>, Runnable {
     }
 
     /**
+     * Sets the secondary priority value for this render task.
+     * This value is used as a criterion to sort equal priority tasks
+     * where the task with the lower secondary priority value will be rendered first.
+     *
+     * @param secondaryPriority the secondary priority value to set
+     */
+    public void setSecondaryPriority(long secondaryPriority) {
+        this.secondaryPriority = secondaryPriority;
+    }
+
+    /**
      * Compares this object with the specified object for order.
      * @return Returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
      */
     @Override
     public int compareTo(RenderTask t1) {
-        return this.renderPriority.getPriority() - t1.renderPriority.getPriority();
+        int comparison = this.renderPriority.getPriority() - t1.renderPriority.getPriority();
+        if (comparison == 0) return (int) (this.secondaryPriority - t1.secondaryPriority);
+        else return comparison;
     }
 }
--- src/main/java/dev/emberline/core/render/Renderer.java
+++ src/main/java/dev/emberline/core/render/Renderer.java
@@ -1,5 +1,6 @@
 package dev.emberline.core.render;
 
+import dev.emberline.gui._TemporarySpriteProvider;
 import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,6 +10,7 @@ import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import dev.emberline.core.components.Renderable;
+import javafx.scene.image.Image;
 
 public class Renderer {
     public static final int GUICS_HEIGHT = 18;
@@ -27,6 +29,7 @@ public class Renderer {
 
     // Rendering queue
     private final PriorityBlockingQueue<RenderTask> renderQueue = new PriorityBlockingQueue<>();
+    private long taskOrderingCounter = 0;
 
     public Renderer(Renderable root, Canvas canvas) {
         this.root = root;
@@ -65,6 +68,7 @@ public class Renderer {
      * @throws NullPointerException if renderTask is null
      */
     public void addRenderTask(RenderTask renderTask) {
+        renderTask.setSecondaryPriority(taskOrderingCounter++);
         renderQueue.offer(Objects.requireNonNull(renderTask));
     }
 
@@ -79,4 +83,55 @@ public class Renderer {
     public CoordinateSystem getGuiCoordinateSystem() {
         return guiCoordinateSystem;
     }
+
+    // UTILITY METHODS FOR RENDERING; must be called from the JavaFX Application Thread (rendertask lambda) //
+    public static void drawImage(Image image, GraphicsContext gc, CoordinateSystem cs, double x, double y, double width, double height) {
+        gc.drawImage(image, cs.toScreenX(x), cs.toScreenY(y), cs.getScale() * width, cs.getScale() * height);
+    }
+
+    // Draw an Image with a fixed aspect ratio, aligned to the left and centered vertically of the given rectangular area
+    public static void drawImageFit(Image image, GraphicsContext gc, CoordinateSystem cs, double x, double y, double width, double height) {
+        double scalingFactor = Math.min(width / image.getWidth(), height / image.getHeight());
+        y += (height - image.getHeight() * scalingFactor) / 2; // vertical centering
+        drawImage(image, gc, cs, x, y, image.getWidth() * scalingFactor, image.getHeight() * scalingFactor);
+    }
+
+    // Draw an Image with a fixed aspect ratio, aligned to the center of the given rectangular area in both axes
+    public static void drawImageFitCenter(Image image, GraphicsContext gc, CoordinateSystem cs, double x, double y, double width, double height) {
+        double scalingFactor = Math.min(width / image.getWidth(), height / image.getHeight());
+        y += (height - image.getHeight() * scalingFactor) / 2; // vertical centering
+        x += (width - image.getWidth() * scalingFactor) / 2; // horizontal centering
+        drawImage(image, gc, cs, x, y, image.getWidth() * scalingFactor, image.getHeight() * scalingFactor);
+    }
+
+    // drawtext centering height margin
+    private static final double CENTER_TEXT_H_MARGIN = 0.07;
+    // Minimum area in pixels before using uppercase
+    private static final double MIN_TEXT_AREA_PX_UPPERCASE = 500;
+    // Minimum text height in pixels before enabling image smoothing
+    private static final double MIN_TEXT_HEIGHT_PX_SMOOTH = 20;
+
+    // Draw a string within the given rectangular area optimizing for readability
+    public static void drawText(String text, GraphicsContext gc, CoordinateSystem cs, double x, double y, double width, double height) {
+        boolean gcImageSmoothing = gc.isImageSmoothing();
+        double areaInPixels = width * height * cs.getScale() * cs.getScale();
+        // Convert to uppercase if the area is too small
+        if (areaInPixels < MIN_TEXT_AREA_PX_UPPERCASE) text = text.toUpperCase();
+        // Use image smoothing if the area is too small
+        if (height * cs.getScale() < MIN_TEXT_HEIGHT_PX_SMOOTH) gc.setImageSmoothing(true);
+        // Fit image or stretch vertically
+        Image image = _TemporarySpriteProvider.getStringImage(text);
+        if (width / image.getWidth() < height / image.getHeight()) {
+            drawImage(image, gc, cs, x, y + height * CENTER_TEXT_H_MARGIN, width, height * (1 - 2 * CENTER_TEXT_H_MARGIN));
+        } else {
+            drawImageFit(image, gc, cs, x, y, width, height);
+        }
+        gc.setImageSmoothing(gcImageSmoothing);
+    }
+
+    // Draw a rectangle with the given coordinates and size, filling it with the current fill color
+    public static void fillRect(GraphicsContext gc, CoordinateSystem cs, double x, double y, double width, double height) {
+        gc.fillRect(cs.toScreenX(x), cs.toScreenY(y), cs.getScale() * width, cs.getScale() * height);
+    }
+
 }
