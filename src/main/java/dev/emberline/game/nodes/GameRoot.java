bcf129314c54c59297815030c3a8cb51e56dca11
Tommaso Goni <tommaso.goni@studio.unibo.it>
2025-06-05 17:20:07 +0200

    Adding dummy Tower class to test TowerDialogLayer

--- src/main/java/dev/emberline/game/nodes/GameRoot.java
+++ src/main/java/dev/emberline/game/nodes/GameRoot.java
@@ -3,6 +3,9 @@ package dev.emberline.game.nodes;
 import dev.emberline.core.components.Inputable;
 import dev.emberline.core.components.Renderable;
 import dev.emberline.core.components.Updatable;
+import dev.emberline.game.world.Tower;
+import dev.emberline.gui.GuiLayer;
+import dev.emberline.gui.towerdialog.TowerDialogLayer;
 import dev.emberline.gui.event.GuiEvent;
 import dev.emberline.gui.event.GuiEventListener;
 import dev.emberline.game.world.World;
@@ -11,21 +14,30 @@ import javafx.scene.input.InputEvent;
 public class GameRoot implements Inputable, Updatable, Renderable, GuiEventListener {
     // Navigation States
     private final World world = new World();
+    private final GuiLayer gl;
 
     public GameRoot() {
+        Tower t = new Tower();
+        gl = new TowerDialogLayer(t);
+        gl.setListener(t);
     }
 
     @Override
-    public void processInput(InputEvent inputEvent) {}
+    public void processInput(InputEvent inputEvent) {
+        gl.processInput(inputEvent);
+    }
 
     @Override
-    public void update(long elapsed) {}
+    public void update(long elapsed) {
+    }
 
     @Override
     public void render() {
-        world.render();
+        gl.render();
     }
 
     @Override
-    public void onGuiEvent(GuiEvent event) {}
+    public void onGuiEvent(GuiEvent event) {
+
+    }
 }
