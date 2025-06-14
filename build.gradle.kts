4b5ba102d0b11f88f4db57688cd766ddf463ad82
Tommaso Bandini <tommaso.bandini5@studio.unibo.it>
2025-06-14 15:39:55 +0200

    Add ConfigLoader class for loading configuration files using Jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
 }
 
 javafx {
