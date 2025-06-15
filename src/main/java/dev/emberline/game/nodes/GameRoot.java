fcbd24a11f039a6fa3c1da07cd58e27f9956e871
Luigi Linari <luigi.linari@studio.unibo.it>
2025-06-15 15:48:41 +0200

    basic serialization istructions only for test purpose

--- src/main/java/dev/emberline/game/nodes/GameRoot.java
+++ src/main/java/dev/emberline/game/nodes/GameRoot.java
@@ -8,11 +8,38 @@ import dev.emberline.gui.event.GuiEventListener;
 import dev.emberline.game.world.World;
 import javafx.scene.input.InputEvent;
 
+import java.io.*;
+
 public class GameRoot implements Inputable, Updatable, Renderable, GuiEventListener {
     // Navigation States
     private final World world = new World();
+    private boolean isSerialized = false;
+
+    private long acc = 0;
 
     public GameRoot() {
+    }
+
+    private void serialize() throws Exception {
+        try (
+            final OutputStream file = new FileOutputStream("pippo");
+            final OutputStream bstream = new BufferedOutputStream(file);
+            final ObjectOutputStream ostream = new ObjectOutputStream(bstream);
+        ) {
+            ostream.writeObject(world);
+            isSerialized = true;
+        }
+    }
+
+    private void deSerialize() throws Exception {
+        try (
+            final InputStream file = new FileInputStream("pippo");
+            final InputStream bstream = new BufferedInputStream(file);
+            final ObjectInputStream ostream = new ObjectInputStream(bstream);
+        ) {
+            ostream.readObject();
+            isSerialized = false;
+        }
     }
 
     @Override
@@ -20,12 +47,30 @@ public class GameRoot implements Inputable, Updatable, Renderable, GuiEventListe
 
     @Override
     public void update(long elapsed) {
-        world.update(elapsed);
+        if (acc > 10_000_000_000L && isSerialized) {
+            try {
+                //deSerialize();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }else if (acc > 5_000_000_000L && !isSerialized) {
+            try {
+                //serialize();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        acc += elapsed;
+        if (!isSerialized) {
+            world.update(elapsed);
+        }
     }
 
     @Override
     public void render() {
-        world.render();
+        if (!isSerialized) {
+            world.render();
+        }
     }
 
     @Override
