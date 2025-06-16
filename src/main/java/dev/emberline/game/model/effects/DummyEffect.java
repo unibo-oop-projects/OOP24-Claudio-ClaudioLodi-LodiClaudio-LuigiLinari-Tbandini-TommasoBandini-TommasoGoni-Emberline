1f461a8aee957a4bb027cda1747ad4711b27333b
Tommaso Goni <tommaso.goni@studio.unibo.it>
2025-06-16 17:51:37 +0200

    Add `DummyEffect` implementation as base enchantment effect placeholder

---
+++ src/main/java/dev/emberline/game/model/effects/DummyEffect.java
@@ -0,0 +1,32 @@
+package dev.emberline.game.model.effects;
+
+import dev.emberline.game.model.EnchantmentInfo;
+import dev.emberline.gui.towerdialog.stats.TowerStat;
+
+import java.util.List;
+
+/**
+ * DummyEffect is a simple implementation of the {@link EnchantmentEffect} interface.
+ * This implementation represents a base enchantment effect that does not provide any
+ * additional behavior, such as modifying enemy behavior or applying specific stats.
+ * It can be used as a placeholder or as the default state for non-upgraded enchantments.
+ * <p>
+ * It is associated with the {@code BASE} enchantment type, which is the default type
+ * without any special effects or upgrade capabilities.
+ */
+public class DummyEffect implements EnchantmentEffect {
+    @Override
+    public boolean isExpired() {
+        return true;
+    }
+
+    @Override
+    public EnchantmentInfo.Type getEnchantmentType() {
+        return EnchantmentInfo.Type.BASE;
+    }
+
+    @Override
+    public List<TowerStat> getTowerStats() {
+        return List.of();
+    }
+}
