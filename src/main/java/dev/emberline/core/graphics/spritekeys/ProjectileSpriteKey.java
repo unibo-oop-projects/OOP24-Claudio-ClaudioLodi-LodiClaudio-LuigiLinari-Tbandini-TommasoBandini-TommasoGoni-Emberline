09b489219324abca7bccf6be73962c4abdc2b0c6
Tommaso Bandini <tommaso.bandini5@studio.unibo.it>
2025-06-18 16:07:06 +0200

    Add ProjectileSpriteKey and TowerSpriteKey records for sprite management

---
+++ src/main/java/dev/emberline/core/graphics/spritekeys/ProjectileSpriteKey.java
@@ -0,0 +1,6 @@
+package dev.emberline.core.graphics.spritekeys;
+
+import dev.emberline.game.model.EnchantmentInfo;
+import dev.emberline.game.model.ProjectileInfo;
+
+public record ProjectileSpriteKey(ProjectileInfo.Type size, EnchantmentInfo.Type enchant) implements SpriteKey {}
