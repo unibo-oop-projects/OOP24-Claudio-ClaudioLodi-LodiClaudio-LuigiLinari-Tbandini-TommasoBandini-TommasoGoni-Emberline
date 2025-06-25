4519b4403e1d26385b0d4df91fd44227fd691cbd
Tommaso Bandini <tommaso.bandini5@studio.unibo.it>
2025-06-25 03:49:17 +0200

    Minor fix

--- src/main/java/dev/emberline/game/world/entities/enemies/enemy/EnemyRenderComponent.java
+++ src/main/java/dev/emberline/game/world/entities/enemies/enemy/EnemyRenderComponent.java
@@ -12,7 +12,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Paint;
 
-class EnemyRenderComponent implements Renderable {
 public class EnemyRenderComponent implements Renderable {
     private final AbstractEnemy enemy;
     private final EnemyAnimation enemyAnimation;
