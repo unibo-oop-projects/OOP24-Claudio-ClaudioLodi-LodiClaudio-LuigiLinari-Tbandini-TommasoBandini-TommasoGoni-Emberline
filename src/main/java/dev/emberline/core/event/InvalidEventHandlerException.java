0489c35ca449f2484ecf3b1c33132819af6e42db
Tommaso Goni <tommaso.goni@studio.unibo.it>
2025-06-26 23:04:36 +0200

    Fixing PMD warnings and errors in `dev.emberline.core.event`

--- src/main/java/dev/emberline/core/event/InvalidEventHandlerException.java
+++ src/main/java/dev/emberline/core/event/InvalidEventHandlerException.java
@@ -1,6 +1,29 @@
 package dev.emberline.core.event;
 
-public class InvalidEventHandlerException extends RuntimeException {
+import java.io.Serial;
+
+/**
+ * This exception is thrown to indicate that an event handler method is invalid
+ * or does not comply with the required structure or constraints expected by
+ * the event dispatching system, described in the {@link EventHandler} documentation.
+ * <p>
+ * The {@code EventHandlerInvocationException} is intended for use
+ * within the context of the event dispatching system, such as in the
+ * {@code EventDispatcher#registerListener(java.util.EventListener)} method.
+ *
+ * @see EventDispatcher#registerListener(java.util.EventListener)
+ * @see EventDispatcher
+ */
+class InvalidEventHandlerException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = -5988139178342694937L;
+
+    /**
+     * Constructs a new {@code EventHandlerInvocationException} with the specified
+     * detail message.
+     *
+     * @param message the detail message explaining the reason for the exception.
+     */
     public InvalidEventHandlerException(final String message) {
         super(message);
     }
