e795102033fe317e7a3858f5a61cbae7fc6bdeab
Tommaso Bandini <tommaso.bandini5@studio.unibo.it>
2025-06-26 02:38:21 +0200

    Add PreferenceKey and PreferencesManager classes for managing user preferences

---
+++ src/main/java/dev/emberline/preferences/PreferenceKey.java
@@ -0,0 +1,39 @@
+package dev.emberline.preferences;
+
+public enum PreferenceKey {
+
+    MUSIC_VOLUME("musicVolume", 0.5),
+    MUSIC_MUTE("musicMute", false),
+    SFX_VOLUME("sfxVolume", 0.5),
+    SFX_MUTE("sfxMute", false),
+    FULLSCREEN("fullscreen", false);
+
+    private final String key;
+    private final Double doubleDefaultValue;
+    private final Boolean booleanDefaultValue;
+
+    PreferenceKey(final String key, final Double doubleDefaultValue) {
+        this.key = key;
+        this.booleanDefaultValue = null;
+        this.doubleDefaultValue = doubleDefaultValue;
+    }
+
+    PreferenceKey(final String key, final Boolean booleanDefaultValue) {
+        this.key = key;
+        this.doubleDefaultValue = null;
+        this.booleanDefaultValue = booleanDefaultValue;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Boolean getDefaultBooleanValue() {
+        return booleanDefaultValue;
+    } 
+    
+    public Double getDefaultDoubleValue() {
+        return doubleDefaultValue;
+    } 
+    
+}
---
+++ src/main/java/dev/emberline/preferences/PreferencesManager.java
@@ -0,0 +1,37 @@
+package dev.emberline.preferences;
+
+import java.util.prefs.Preferences;
+
+public final class PreferencesManager {
+
+    private static Preferences prefs = Preferences.userRoot().node("dev.emberline.preferences");
+
+    private PreferencesManager() {
+    }
+
+    public static Double getDoublePreference(final PreferenceKey key) {
+        double value = prefs.getDouble(key.getKey(), -1);
+        if (value == -1) {
+            prefs.putDouble(key.getKey(), value = key.getDefaultDoubleValue());
+        }
+        return value;
+    }
+
+    public static Boolean getBooleanPreference(final PreferenceKey key) {
+        boolean value = prefs.getBoolean(key.getKey(), key.getDefaultBooleanValue());
+        if (value == key.getDefaultBooleanValue()) {
+            prefs.putBoolean(key.getKey(), value = key.getDefaultBooleanValue());
+
+        }
+        System.out.println(prefs.get(key.getKey(), "ciao"));
+        return value;
+    }
+
+    public static void setDoublePreference(final PreferenceKey key, final Double value) {
+        setDoublePreference(key, value);
+    }
+
+    public static void setBooleanPreference(final PreferenceKey key, final Boolean value) {
+        setBooleanPreference(key, value);
+    }
+}
