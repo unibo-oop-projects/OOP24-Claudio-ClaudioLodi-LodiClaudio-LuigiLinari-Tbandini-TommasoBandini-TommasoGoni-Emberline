81fdd7c50bee8195e7c82f0a2651bf698de82a65
Luigi Linari <luigi.linari@studio.unibo.it>
2025-06-28 12:10:43 +0200

    test usage of Serializer class in GameRoot

--- src/main/java/dev/emberline/game/GameRoot.java
+++ src/main/java/dev/emberline/game/GameRoot.java
@@ -42,9 +42,7 @@ public class GameRoot implements Inputable, Updatable, Renderable, EventListener
     private GameState currentState;
     private GameState previousState;
 
-    private String _fileName = "pluto";
-    private long acc = 0;
-    private boolean isSerialized;
+    private final Serializer worldSerializer = new Serializer(world);
     /**
      * Constructs a new instance of {@code GameRoot} and initializes the main menu
      * as the current game state.
@@ -62,45 +60,13 @@ public class GameRoot implements Inputable, Updatable, Renderable, EventListener
         currentState.processInput(inputEvent);
     }
 
-    private void serialize() {
-        try {
-            final OutputStream file = new FileOutputStream(_fileName);
-            final OutputStream bstream = new BufferedOutputStream(file);
-            final ObjectOutputStream ostream = new ObjectOutputStream(bstream);
-
-            ostream.writeObject(world);
-        } catch (IOException ex) {
-            System.out.println("serialization issue from: " + ex.getMessage());
-        }
-    }
-
-    private void deserialize() {
-        try {
-            final InputStream file = new FileInputStream(_fileName);
-            final InputStream bstream = new BufferedInputStream(file);
-            final ObjectInputStream ostream = new ObjectInputStream(bstream);
-
-            world = (World) ostream.readObject();
-        } catch (IOException | ClassNotFoundException ex) {
-            System.out.println("deserializaion issue from: " + ex.getMessage());
-        }
-    }
-
     /**
      * {@inheritDoc}
      */
     @Override
     public void update(final long elapsed) {
-        long nsToSeconds = 1_000_000_000;
-        acc += elapsed;
-        long tmp = acc / nsToSeconds;
-        if (currentState == world && (tmp >= 10 && tmp <= 20 )) {
-            if (tmp >= 19 && isSerialized) {
-                //deserialize();
-            } else if (!isSerialized) {
-                serialize();
-                isSerialized = true;
-            }
+        if (currentState == world) {
+            worldSerializer.update(elapsed);
         } else
             currentState.update(elapsed);
     }
@@ -110,7 +76,10 @@ public class GameRoot implements Inputable, Updatable, Renderable, EventListener
      */
     @Override
     public void render() {
-        currentState.render();
+        if (currentState == world) {
+            worldSerializer.render();
+        } else
+            currentState.render();
     }
 
     // Event Handlers
