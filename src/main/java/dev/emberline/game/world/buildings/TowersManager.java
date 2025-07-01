de14c3a1787ec294076465aac24fe17b366b9d38
Luigi Linari <luigi.linari@studio.unibo.it>
2025-07-02 01:23:14 +0200

    removed import with star

--- src/main/java/dev/emberline/game/world/buildings/TowersManager.java
+++ src/main/java/dev/emberline/game/world/buildings/TowersManager.java
@@ -19,7 +19,11 @@ import javafx.scene.input.MouseEvent;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.List;
 
 /**
  * Manages the lifecycle and interactions of towers and pre-built tower in the world.
