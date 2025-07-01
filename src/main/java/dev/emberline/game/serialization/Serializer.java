2d3e3490f65fe44a15e7d73040d03079a36eaad9
Luigi Linari <luigi.linari@studio.unibo.it>
2025-07-02 01:23:56 +0200

    safer stream usage, about closing them even when exceptions occur

--- src/main/java/dev/emberline/game/serialization/Serializer.java
+++ src/main/java/dev/emberline/game/serialization/Serializer.java
@@ -4,12 +4,13 @@ import dev.emberline.game.world.World;
 import net.harawata.appdirs.AppDirs;
 import net.harawata.appdirs.AppDirsFactory;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Utility class that should be used to serialize the world - save the game.
@@ -37,18 +38,12 @@ public class Serializer {
      */
     public void serialize(final World world, final String filename) {
         try {
-            final File saveDir = new File(SAVE_PATH);
-            if (!saveDir.exists()) {
-                saveDir.mkdirs();
+            Files.createDirectories(Path.of(SAVE_PATH));
+
+            try (ObjectOutputStream out = new ObjectOutputStream(
+                    new FileOutputStream(SAVE_PATH + filename))) {
+                out.writeObject(world);
             }
-
-            final FileOutputStream file = new FileOutputStream(SAVE_PATH + filename);
-            final ObjectOutputStream out = new ObjectOutputStream(file);
-
-            out.writeObject(world);
-
-            out.close();
-            file.close();
         } catch (IOException ex) {
             throw new GameSavesSavingException(ex);
         }
@@ -63,13 +58,11 @@ public class Serializer {
     public World getDeserializedWorld(final String save) {
         final World world;
         try {
-            final FileInputStream file = new FileInputStream(SAVE_PATH + save);
-            final ObjectInputStream in = new ObjectInputStream(file);
+            final ObjectInputStream in = new ObjectInputStream(new FileInputStream(SAVE_PATH + save));
 
             world = (World) in.readObject();
 
             in.close();
-            file.close();
         } catch (IOException | ClassNotFoundException ex) {
             throw new GameSavesLoadingException(ex);
         }
