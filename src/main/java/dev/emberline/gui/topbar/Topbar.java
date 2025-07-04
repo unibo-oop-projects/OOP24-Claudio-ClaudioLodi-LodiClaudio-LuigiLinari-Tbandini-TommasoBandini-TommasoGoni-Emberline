e507461fc20cc980676b48fcc720f8dd7556ae05
Tommaso Bandini <tommaso.bandini5@studio.unibo.it>
2025-07-04 15:42:57 +0200

    Changed Layout instance to static.

--- src/main/java/dev/emberline/gui/topbar/Topbar.java
+++ src/main/java/dev/emberline/gui/topbar/Topbar.java
@@ -32,9 +32,9 @@ public class Topbar extends GuiLayer implements EventListener {
     private int gold;
     private int wave;
     private final World world;
-    private static final Layout layout = ConfigLoader.loadConfig("/gui/topbar/topbarLayout.json", Layout.class);
+    private static final Layout LAYOUT = ConfigLoader.loadConfig("/gui/topbar/topbarLayout.json", Layout.class);
 
-    private record Layout (
+    private record Layout(
             @JsonProperty
             double bgWidth,
             @JsonProperty
@@ -70,10 +70,9 @@ public class Topbar extends GuiLayer implements EventListener {
      * @see Topbar
      */
     public Topbar(final World world) {
-        super(layout.bgX, layout.bgY, layout.bgWidth, layout.bgHeight);
+        super(LAYOUT.bgX, LAYOUT.bgY, LAYOUT.bgWidth, LAYOUT.bgHeight);
 
         registerEvents();
-
         this.world = world;
     }
 
@@ -93,8 +92,8 @@ public class Topbar extends GuiLayer implements EventListener {
     }
 
     private void addOptionsButton() {
-        final GuiButton optionsButton = new GuiButton(layout.btnOptionsX, layout.btnOptionsY,
-                layout.btnOptionsWidth, layout.btnOptionsHeight,
+        final GuiButton optionsButton = new GuiButton(LAYOUT.btnOptionsX, LAYOUT.btnOptionsY,
+                LAYOUT.btnOptionsWidth, LAYOUT.btnOptionsHeight,
                 SpriteLoader.loadSprite(SingleSpriteKey.TOPBAR_OPTIONS_BUTTON).image(),
                 SpriteLoader.loadSprite(SingleSpriteKey.TOPBAR_OPTIONS_BUTTON_HOVER).image());
         optionsButton.setOnClick(() -> throwEvent(new OpenOptionsEvent(optionsButton)));
@@ -103,9 +102,9 @@ public class Topbar extends GuiLayer implements EventListener {
 
     private void drawStats(final GraphicsContext gc, final CoordinateSystem cs,
                            final Image healtImage, final Image goldImage, final Image waveImage) {
-        drawStatImage(gc, cs, healtImage, layout.statsXHealth, layout.statsHeight);
-        drawStatImage(gc, cs, goldImage, layout.statsXGold, layout.statsHeight);
-        drawStatImage(gc, cs, waveImage, layout.statsXWave, layout.statsHeight);
+        drawStatImage(gc, cs, healtImage, LAYOUT.statsXHealth, LAYOUT.statsHeight);
+        drawStatImage(gc, cs, goldImage, LAYOUT.statsXGold, LAYOUT.statsHeight);
+        drawStatImage(gc, cs, waveImage, LAYOUT.statsXWave, LAYOUT.statsHeight);
     }
 
     private void drawStatImage(final GraphicsContext gc, final CoordinateSystem cs,
@@ -113,7 +112,7 @@ public class Topbar extends GuiLayer implements EventListener {
         final double ratio = img.getWidth() / img.getHeight();
         final double targetHeight = baseHeight * 0.8;
         final double targetWidth = targetHeight * ratio;
-        Renderer.drawImage(img, gc, cs, x, (layout.bgHeight - targetHeight) / 2, targetWidth, targetHeight);
+        Renderer.drawImage(img, gc, cs, x, (LAYOUT.bgHeight - targetHeight) / 2, targetWidth, targetHeight);
     }
 
     /**
@@ -134,7 +133,7 @@ public class Topbar extends GuiLayer implements EventListener {
         renderer.addRenderTask(new RenderTask(RenderPriority.GUI, () -> {
             // Background
             Renderer.drawImage(SpriteLoader.loadSprite(SingleSpriteKey.TOPBAR_BACKGROUND).image(),
-                    gc, guics, layout.bgX, layout.bgY, layout.bgWidth, layout.bgHeight);
+                    gc, guics, LAYOUT.bgX, LAYOUT.bgY, LAYOUT.bgWidth, LAYOUT.bgHeight);
             // Stats
             drawStats(gc, guics, healthImageString, goldImageString, waveImageString);
         }));
